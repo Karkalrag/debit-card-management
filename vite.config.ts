0908fc915fd686228abf203f09b1924d865f09a8
Karthik R <karkalrag@gmail.com>
2022-10-08 00:28:01 +0530

    add quasar framework and related dependancies

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { fileURLToPath, URL } from "node:url";
+import { quasar, transformAssetUrls } from "@quasar/vite-plugin";
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
@@ -6,7 +7,15 @@ import vueJsx from "@vitejs/plugin-vue-jsx";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  plugins: [
+    vue({
+      template: { transformAssetUrls },
+    }),
+    quasar({
+      sassVariables: "src/quasar-variables.sass",
+    }),
+    vueJsx(),
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
