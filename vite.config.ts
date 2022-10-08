bcfb4dafe04e55ae9ed36caf85e2ceb9cc2d0d1b
Karthik R <karkalrag@gmail.com>
2022-10-08 10:08:52 +0530

    revert quasar config

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,4 @@
 import { fileURLToPath, URL } from "node:url";
-import { quasar, transformAssetUrls } from "@quasar/vite-plugin";
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
@@ -7,15 +6,7 @@ import vueJsx from "@vitejs/plugin-vue-jsx";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue({
-      template: { transformAssetUrls },
-    }),
-    quasar({
-      sassVariables: "src/quasar-variables.sass",
-    }),
-    vueJsx(),
-  ],
+  plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
